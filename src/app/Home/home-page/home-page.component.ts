8c460d35ccacbc232ca5ac8f42d37a616c00f693
nouranmostafa235 <nourmostafa2197@gmail.com>
2024-06-23 22:47:36 +0300

    commit

--- src/app/Home/home-page/home-page.component.ts
+++ src/app/Home/home-page/home-page.component.ts
@@ -1,51 +1,28 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { OwlOptions } from 'ngx-owl-carousel-o';
+import { Component,OnInit, Renderer2, HostListener } from '@angular/core';
 
-declare const $:any;
-declare const WOW:any;
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
-  onTranslated(): void {
-    const activeSlides = document.querySelectorAll('.owl-item.active .slide-in-left');
-    activeSlides.forEach(slide => {
-      const element = slide as HTMLElement;
-      element.classList.remove('slide-in-left');
-      void element.offsetWidth;
-      element.classList.add('slide-in-left');
-    });
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    this.checkScroll();
   }
-  customOptions: OwlOptions = {
-    loop: true,
-    autoplay:true, 
-    slideTransition: 'linear',
-    autoplayTimeout:7000,
-    autoplaySpeed:2000,
-    autoplayHoverPause:false,
-    autoWidth:true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: false,
-    navSpeed: 700,
-    navText: ['', ''],
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 1
-      },
-      740: {
-        items: 1
-      },
-      940: {
-        items: 1
-      }
-    },
-    nav: true,
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    this.checkScroll();
+  }
+
+  private checkScroll(): void {
+    const navbar = document.getElementsByClassName('text')[0];
+    if (window.scrollY < 50) {
+      this.renderer.addClass(navbar, 'slide-in-left');
+    } else {
+      this.renderer.removeClass(navbar, 'slide-in-left');
+    }
   }
 }
--- src/app/NavBar/navbar/navbar.component.ts
+++ src/app/NavBar/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  constructor(private renderer: Renderer2) { }
 
+  ngOnInit(): void {
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    this.checkScroll();
+  }
+
+  private checkScroll(): void {
+    const navbar = document.getElementsByClassName('navbar')[0];
+    if (window.scrollY < 50) {
+      this.renderer.addClass(navbar, 'bg-transparent');
+    } else {
+      this.renderer.removeClass(navbar, 'bg-transparent');
+    }
+  }
 }
