e4a34eb2d9008625f0ea9dd46d72b5a4a0da0ec7
wanuus <82518131+wanuus@users.noreply.github.com>
2024-07-03 19:40:40 +0300

    admin-login
    //

--- src/app/Admin/admin-login/admin-login.component.ts
+++ src/app/Admin/admin-login/admin-login.component.ts
@@ -1,32 +1,3 @@
-// import { Component } from '@angular/core';
-// import { FormControl, FormGroup, Validators } from '@angular/forms';
-// import { Router } from '@angular/router';
-// import { SignInService } from 'src/app/sign-in.service';
-
-// @Component({
-//   selector: 'app-admin-login',
-//   templateUrl: './admin-login.component.html',
-//   styleUrls: ['./admin-login.component.css']
-// })
-// export class AdminLoginComponent {
-//   constructor(private router:Router, private _service:SignInService){}
-//   login:FormGroup = new FormGroup({
-//     username:new FormControl(null,[Validators.required,Validators.email]),
-//     password:new FormControl(null,[Validators.required]),
-//   })
-//   handleLogin(form:FormGroup){
-//      this._service.adminLogin(form.value).subscribe({
-//       next:(response)=>{
-//         if(response.message === "Sign in successful"){
-//           localStorage.setItem('Admintoken',"Bearer "+response.token)
-//           this._service.decodeAdminToken()
-//           this.router.navigate(['/adminHomePage'])
-//         }
-//       },
-//       error:(err)=>console.log(err)
-//      })
-//   }
-// }
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -41,7 +12,7 @@ export class AdminLoginComponent {
   constructor(private router: Router, private _service: SignInService) { }
   login: FormGroup = new FormGroup({
     username: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.minLength(8), Validators.pattern(/^[A-Z] [a-z0-9]{5,10}$/), Validators.required]),
   })
   handleLogin(form: FormGroup) {
     this._service.adminLogin(form.value).subscribe({
