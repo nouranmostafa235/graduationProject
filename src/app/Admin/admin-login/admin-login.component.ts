399b10a7bc923cd7b31d0d41fb21da92b2555fe6
wanuus <82518131+wanuus@users.noreply.github.com>
2024-07-03 19:55:24 +0300

    admin
    mm

--- src/app/Admin/admin-login/admin-login.component.ts
+++ src/app/Admin/admin-login/admin-login.component.ts
@@ -12,18 +12,18 @@ export class AdminLoginComponent {
   constructor(private router: Router, private _service: SignInService) { }
   login: FormGroup = new FormGroup({
     username: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
   })
-  handleLogin(form:FormGroup){
-     this._service.adminLogin(form.value).subscribe({
-      next:(response)=>{
-        if(response.message === "Sign in successful"){
-          localStorage.setItem('Admintoken',"Bearer "+response.token)
+  handleLogin(form: FormGroup) {
+    this._service.adminLogin(form.value).subscribe({
+      next: (response) => {
+        if (response.message === "Sign in successful") {
+          localStorage.setItem('Admintoken', "Bearer " + response.token)
           this._service.decodeAdminToken()
           this.router.navigate(['/adminHomePage'])
         }
       },
-      error:(err)=>console.log(err)
-     })
+      error: (err) => console.log(err)
+    })
   }
 }
