a546e04017600e23b5084f093f7420d745598e0d
Nohhaa <96923194+Nohhaa@users.noreply.github.com>
2024-07-03 19:31:16 +0300

    Revert "admin_login"
    This reverts commit f1a067b9832eec6b73b7c1c00eae07d2f2f706da.

--- src/app/Admin/admin-login/admin-login.component.ts
+++ src/app/Admin/admin-login/admin-login.component.ts
@@ -1,32 +1,3 @@
-// import { Component } from '@angular/core';
-// import { FormControl, FormGroup, Validators } from '@angular/forms';
-// import { Router } from '@angular/router';
-// import { SignInService } from 'src/app/sign-in.service';
-
-// @Component({
-//   selector: 'app-admin-login',
-//   templateUrl: './admin-login.component.html',
-//   styleUrls: ['./admin-login.component.css']
-// })
-// export class AdminLoginComponent {
-//   constructor(private router:Router, private _service:SignInService){}
-//   login:FormGroup = new FormGroup({
-//     username:new FormControl(null,[Validators.required,Validators.email]),
-//     password:new FormControl(null,[Validators.required]),
-//   })
-//   handleLogin(form:FormGroup){
-//      this._service.adminLogin(form.value).subscribe({
-//       next:(response)=>{
-//         if(response.message === "Sign in successful"){
-//           localStorage.setItem('Admintoken',"Bearer "+response.token)
-//           this._service.decodeAdminToken()
-//           this.router.navigate(['/adminHomePage'])
-//         }
-//       },
-//       error:(err)=>console.log(err)
-//      })
-//   }
-// }
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -38,21 +9,21 @@ import { SignInService } from 'src/app/sign-in.service';
   styleUrls: ['./admin-login.component.css']
 })
 export class AdminLoginComponent {
-  constructor(private router: Router, private _service: SignInService) { }
-  login: FormGroup = new FormGroup({
-    username: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null, [Validators.required]),
+  constructor(private router:Router, private _service:SignInService){}
+  login:FormGroup = new FormGroup({
+    username:new FormControl(null,[Validators.required,Validators.email]),
+    password:new FormControl(null,[Validators.required]),
   })
-  handleLogin(form: FormGroup) {
-    this._service.adminLogin(form.value).subscribe({
-      next: (response) => {
-        if (response.message === "Sign in successful") {
-          localStorage.setItem('Admintoken', "Bearer " + response.token)
+  handleLogin(form:FormGroup){
+     this._service.adminLogin(form.value).subscribe({
+      next:(response)=>{
+        if(response.message === "Sign in successful"){
+          localStorage.setItem('Admintoken',"Bearer "+response.token)
           this._service.decodeAdminToken()
           this.router.navigate(['/adminHomePage'])
         }
       },
-      error: (err) => console.log(err)
-    })
+      error:(err)=>console.log(err)
+     })
   }
 }
